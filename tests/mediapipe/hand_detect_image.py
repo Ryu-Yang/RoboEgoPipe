d0122151ce1361f4ce1f41b347d8bca2c1a0beb3
Ryu-Yang <Ryu-Yang@qq.com>
2026-04-10 15:16:13 +0800

    refactor(mediapipe): reorganize utilities and add test modules
    - Rename `test_mediapipe.py` to `mediapipe/utils.py` and clean up code
    - Remove hardcoded paths and test logic to create a reusable utility module
    - Add new test files `hand_detect_image.py` and `hand_detect_video.py` for isolated testing
    - Improve project structure by separating core functionality from testing code

---
+++ tests/mediapipe/hand_detect_image.py
@@ -0,0 +1,26 @@
+import cv2
+import mediapipe as mp
+
+from roboegopipe.mediapipe.utils import draw_landmarks_on_image
+
+
+model_path = './models/mediapipe/hand_landmarker.task'
+image_path = './data/test_mediapipe.png'
+
+
+base_options = mp.tasks.BaseOptions(model_asset_path=model_path)
+options = mp.tasks.vision.HandLandmarkerOptions(
+    base_options=base_options,
+    running_mode=mp.tasks.vision.RunningMode.IMAGE,
+    num_hands=2
+    )
+landmarker = mp.tasks.vision.HandLandmarker.create_from_options(options)
+
+mp_image = mp.Image.create_from_file(image_path)
+
+hand_landmarker_result = landmarker.detect(mp_image)
+
+print("result: ", hand_landmarker_result)
+annotated_image = draw_landmarks_on_image(mp_image.numpy_view(), hand_landmarker_result)
+cv2.imshow("test", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+cv2.waitKey(0)
